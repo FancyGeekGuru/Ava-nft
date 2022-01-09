b4dcebf21a41dccc79b7b2620100bd392b11dbff
fancydev0225 <fancydev0225@gmail.com>
2022-01-09 23:26:37 +0300

    0.1.1
    upload files to pinata
    + public key in env

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -5,3 +5,6 @@ const web3 = createAlchemyWeb3(API_URL);
 
 const contract = require("../artifacts/contracts/AvaNFT.sol/AvaNFT.json");
 console.log(JSON.stringify(contract.abi));
+
+const contractAddress = "0xe1437ce9afc37e46f103C356B9E7c9F7567425f0";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
