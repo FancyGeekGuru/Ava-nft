58c4197ffd55d477d11fba98c654a6bb57ed09ab
fancydev0225 <fancydev0225@gmail.com>
2022-01-09 21:45:28 +0300

    0.0.2

--- contracts/AvaNft.sol
+++ contracts/AvaNft.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract MyNFT is ERC721, Ownable {
+contract AvaNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("MyNFT", "NFT") {}
+    constructor() public ERC721("AvaNFT", "AvaNFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
