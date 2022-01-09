d0da9ca77582b4c988956bd03e7144be5dc08886
fancydev0225 <fancydev0225@gmail.com>
2022-01-09 23:29:17 +0300

    0.2.2
    add transaction

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const API_URL = process.env.API_URL;
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const {
+    createAlchemyWeb3
+} = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
 
 const contract = require("../artifacts/contracts/AvaNFT.sol/AvaNFT.json");
@@ -8,3 +10,17 @@ console.log(JSON.stringify(contract.abi));
 
 const contractAddress = "0xe1437ce9afc37e46f103C356B9E7c9F7567425f0";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+    //the transaction
+    const tx = {
+        'from': PUBLIC_KEY,
+        'to': contractAddress,
+        'nonce': nonce,
+        'gas': 500000,
+        'maxPriorityFeePerGas': 1999999987,
+        'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    };
+}
