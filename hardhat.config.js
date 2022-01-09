eb45464beea45b66b8ae3c23acd57de732a28b8a
fancydev0225 <fancydev0225@gmail.com>
2022-01-09 21:54:13 +0300

    0.0.2
    + dotnev, +/- hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+// module.exports = {
+//   solidity: "0.8.4",
+// };
+
+
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.4",
-};
+   solidity: "0.7.3",
+   defaultNetwork: "kovan",
+   networks: {
+      hardhat: {},
+      kovan: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
